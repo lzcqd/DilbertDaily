e27808c67329d425c0b0a67a73120a2353ad6d07
LiZichen <lizichen.qd@gmail.com>
2013-04-04 23:21:41 +0800

    Update Layout

--- DilbertDaily/src/com/zic/dilbertdaily/ui/StripFragment.java
+++ DilbertDaily/src/com/zic/dilbertdaily/ui/StripFragment.java
@@ -2,11 +2,14 @@ package com.zic.dilbertdaily.ui;
 
 import com.zic.dilbertdaily.R;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class StripFragment extends Fragment  {
 	private static final String TAG = "StripFragment";
@@ -23,6 +26,10 @@ public class StripFragment extends Fragment  {
 	public View onCreateView(
 			LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		final View v = inflater.inflate(R.layout.strip_fragment, container, false);
+		
+		final ImageView stripContainer = (ImageView) v.findViewById(R.id.stripContainer);
+		stripContainer.setImageBitmap(BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.empty_photo));
+		
 		return v;
 	}
 }
